8c1483e413933722c8a9b22490ea456bd67d51ec
Martin Brychta <martin@brychta.name>
2019-02-04 15:55:46 +0100

    Fix usage with webpack dev server

--- src/index.js
+++ src/index.js
@@ -121,6 +121,10 @@ class GoogleFontsWebpackPlugin {
   }
 
   async requestFontFile (fontUrl) {
+    if (fontUrl.startsWith('"data:application/')) {
+      return fontUrl
+    }
+
     const format = fontUrl.match(new RegExp('(' + Object.values(this.options.formats).join('|') + ')$'))[1]
     const font = await this.requestFont(fontUrl, format)
     return `"data:application/x-font-${format};base64,${Buffer.from(font, 'binary').toString('base64')}"`
